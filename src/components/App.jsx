ffa3ec56c1484f12dc5a4bbae21882306eb9c699
Husamoa <pawebial@gmail.com>
2018-12-30 20:22:09 +0100

    start react-burger-menu

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,25 +2,20 @@
 import React, { Component, Fragment } from 'react';
 import { HashRouter, Route, Switch } from "react-router-dom";
 
-import Start from './Start'
-import NotFound from './NotFound'
+import Start from './Start';
+import Menu from './Menu'
+import NotFound from './NotFound';
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                <HashRouter>
-                    <Fragment>
-                        <Switch>
-                            <Route exact path='/' component={Start} />
-                            <Route path='/0' component={Start} />
-                            <Route path='/2' component={NotFound} />
-                            <Route path='/4' component={NotFound} />
-                            <Route path='/5' component={NotFound} />
-                            <Route component={NotFound} />
-                        </Switch>
-                    </Fragment>
-                </HashRouter>
+              <div id="outer-container">
+                <Menu pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" } />
+                <main id="page-wrap">
+                    <Start/>
+                </main>
+              </div>
             </div>
         );
     }
