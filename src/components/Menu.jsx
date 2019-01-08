514494fd9d8e6f499e3775cdef2860ddfeaa7fd9
Husamoa <pawebial@gmail.com>
2019-01-08 19:57:41 +0100

    add replace to fix warning

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,27 +1,27 @@
 import React, {Component, Fragment} from 'react';
 import {scaleRotate as Menu} from 'react-burger-menu'
-import {Link, HashRouter, Route, Switch} from "react-router-dom";
+import {NavLink, HashRouter, Route, Switch} from "react-router-dom";
 
 
 export default class BugerMenu extends Component {
   render() {
     return (<Fragment>
       <Menu isOpen={false} pageWrapId={"page-wrap"} outerContainerId={"outer-container"}>
-        <Link className="menu-item" to={"/"}>
+        <NavLink className="menu-item" to={"/"} replace>
           Home
-        </Link>
+        </NavLink>
 
-        <Link className="menu-item" to={"/about"}>
+        <NavLink className="menu-item" to={"/about"} replace>
           About
-        </Link>
+        </NavLink>
 
-        <Link className="menu-item" to={"/portfolio"}>
+        <NavLink className="menu-item" to={"/portfolio"} replace>
           Portfolio
-        </Link>
+        </NavLink>
 
-        <Link className="menu-item" to={"/contact"}>
+        <NavLink className="menu-item" to={"/contact"} replace>
           Contact
-        </Link>
+        </NavLink>
       </Menu>
     </Fragment>);
   }
