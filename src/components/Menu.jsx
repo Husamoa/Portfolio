546584d13efd7c5d9a9c9430e914e73281c1f506
Husamoa <pawebial@gmail.com>
2019-01-09 16:38:13 +0100

    add font fontawesome icons

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,26 +1,34 @@
 import React, {Component, Fragment} from 'react';
-import {scaleRotate as Menu} from 'react-burger-menu'
+import {stack as Menu} from 'react-burger-menu'
 import {NavLink, HashRouter, Route, Switch} from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar, faUser, faHome, faPhone, faFile } from '@fortawesome/free-solid-svg-icons'
 
 
 export default class BugerMenu extends Component {
   render() {
     return (<Fragment>
       <Menu isOpen={false} pageWrapId={"page-wrap"} outerContainerId={"outer-container"}>
+        <h1 id={'logo'} className={'h4'}><FontAwesomeIcon icon={faStar} />
+        <span>Paweł Białek</span></h1>
         <NavLink className="menu-item" to={"/"} replace>
-          Home
+          <FontAwesomeIcon icon={faHome} />
+          <span>Home</span>
         </NavLink>
 
         <NavLink className="menu-item" to={"/about"} replace>
-          About
+          <FontAwesomeIcon icon={faUser} />
+          <span>About</span>
         </NavLink>
 
         <NavLink className="menu-item" to={"/portfolio"} replace>
-          Portfolio
+          <FontAwesomeIcon icon={faFile} />
+          <span>Portfolio</span>
         </NavLink>
 
         <NavLink className="menu-item" to={"/contact"} replace>
-          Contact
+          <FontAwesomeIcon icon={faPhone} />
+          <span>Contact</span>
         </NavLink>
       </Menu>
     </Fragment>);
