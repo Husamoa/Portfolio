f84910cc9c2863b710eb653827b82031de9614e2
Husamoa <pawebial@gmail.com>
2019-01-20 09:38:46 +0100

    difference between desktop and other media menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,19 +7,15 @@ export default class Menu extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      navClass: "",
+      navClass: ""
     };
   }
 
   changeNavClass = () => {
-    if(this.state.navClass === "") {
-      this.setState({
-        navClass: "open"
-      })
+    if (this.state.navClass === "") {
+      this.setState({navClass: "open"})
     } else {
-      this.setState({
-        navClass: ""
-      })
+      this.setState({navClass: ""})
     }
   }
 
@@ -28,51 +24,58 @@ export default class Menu extends Component {
     collapse.classList.remove("show");
   }
 
-
   render() {
     return (<Fragment>
-      <nav className="navbar navbar-expand-xl navbar-light bg-light">
-        <NavLink className="navbar-brand" to={"/"}>
-          <FontAwesomeIcon icon={faStar}/>
-          <span>Paweł Białek</span>
-        </NavLink>
-        <div onClick={this.changeNavClass} className="container-fluid">
-          <div id="nav-icon" className={this.state.navClass} type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-          </div>
-          <div className={`collapse navbar-collapse`} id="navbarNav" onClick={this.closeMenu}>
-            <ul className="navbar-nav" >
-              <li className="nav-item">
-                <NavLink className="nav-link js-scroll-trigger" to={"/home"}>
-                  <FontAwesomeIcon icon={faHome}/>
-                  <span>Home</span>
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link js-scroll-trigger" to={"/about"}>
-                  <FontAwesomeIcon icon={faUser}/>
-                  <span>About</span>
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link js-scroll-trigger" to={"/portfolio"}>
-                  <FontAwesomeIcon icon={faFile}/>
-                  <span>Portfolio</span>
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link js-scroll-trigger" to={"/contact"}>
-                  <FontAwesomeIcon icon={faPhone}/>
-                  <span>Contact</span>
-                </NavLink>
-              </li>
-            </ul>
-          </div>
+      <div className="menu">
+        <div className="desktop-menu">
+          <h1 className="text-uppercase text-center">
+            <strong>Front-end developer</strong>
+          </h1>
+          <hr/>
         </div>
-      </nav>
+        <nav className="navbar navbar-expand-xl navbar-light bg-light">
+          <NavLink className="navbar-brand" to={"/"}>
+            <FontAwesomeIcon icon={faStar}/>
+            <span>Paweł Białek</span>
+          </NavLink>
+          <div onClick={this.changeNavClass} className="container-fluid">
+            <div id="nav-icon" className={this.state.navClass} type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <div className={`collapse navbar-collapse`} id="navbarNav" onClick={this.closeMenu}>
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <NavLink className="nav-link js-scroll-trigger" to={"/home"}>
+                    <FontAwesomeIcon icon={faHome}/>
+                    <span>Home</span>
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link js-scroll-trigger" to={"/about"}>
+                    <FontAwesomeIcon icon={faUser}/>
+                    <span>About</span>
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link js-scroll-trigger" to={"/portfolio"}>
+                    <FontAwesomeIcon icon={faFile}/>
+                    <span>Portfolio</span>
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link js-scroll-trigger" to={"/contact"}>
+                    <FontAwesomeIcon icon={faPhone}/>
+                    <span>Contact</span>
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
     </Fragment>);
   }
 }
