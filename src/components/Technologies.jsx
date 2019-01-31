5208ee4c8dd54c3de39c1c0822fb46cbdbc86811
Husamoa <pawebial@gmail.com>
2019-01-31 18:07:35 +0100

    add transition on hover

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -5,57 +5,59 @@ const Technologies = () => {
     <div className="container">
       <div className="row">
         <div className="col-lg-12 text-center">
-          <h2 className="section-heading">Skills</h2>
-          <hr className="my-4"/>
+          <h1>
+            <strong>Skills</strong>
+          </h1>
+          <hr/>
         </div>
       </div>
       <div className="row">
         <div className="col-lg-3 col-md-6 text-center">
-          <div className="service-box mt-5 mx-auto">
-            <img className="sr-button" src="images/icons/html.png" height="100"/>
+          <div className="sr-button mt-5 mx-auto">
+            <img src="images/icons/html.png" height="100"/>
             <h3 className="mb-3">HTML5</h3>
           </div>
         </div>
         <div className="col-lg-3 col-md-6 text-center">
-          <div className="service-box mt-5 mx-auto">
-            <img className="sr-button" src="images/icons/css.png" height="100" alt="Logo CSS"/>
+          <div className="sr-button service-box mt-5 mx-auto">
+            <img src="images/icons/css.png" height="100" alt="Logo CSS"/>
             <h3 className="mb-3">CSS 3</h3>
           </div>
         </div>
         <div className="col-lg-3 col-md-6 text-center">
-          <div className="service-box mt-5 mx-auto">
-            <img className="sr-button" src="images/icons/sass.png" height="100" alt="Logo SASS"/>
+          <div className="sr-button service-box mt-5 mx-auto">
+            <img src="images/icons/sass.png" height="100" alt="Logo SASS"/>
             <h3 className="mb-3">SASS</h3>
           </div>
         </div>
         <div className="col-lg-3 col-md-6 text-center">
-          <div className="service-box mt-5 mx-auto">
-            <img className="sr-button" src="images/icons/bootstrap.png" height="100" alt="Logo Bootstrap"/>
+          <div className="sr-button service-box mt-5 mx-auto">
+            <img src="images/icons/bootstrap.png" height="100" alt="Logo Bootstrap"/>
             <h3 className="mb-3">Bootstrap</h3>
           </div>
         </div>
         <div className="col-lg-3 col-md-6 text-center">
-          <div className="service-box mt-5 mx-auto">
-            <img className="sr-button" src="images/icons/js.png" height="100" alt="Logo JavaScript"/>
+          <div className="sr-button service-box mt-5 mx-auto">
+            <img src="images/icons/js.png" height="100" alt="Logo JavaScript"/>
             <h3 className="mb-3">JavaScript</h3>
           </div>
         </div>
         <div className="col-lg-3 col-md-6 text-center">
-          <div className="service-box mt-5 mx-auto">
-            <img className="sr-button" src="images/icons/jquery.png" height="100" alt="Logo JQuery"/>
+          <div className="sr-button service-box mt-5 mx-auto">
+            <img src="images/icons/jquery.png" height="100" alt="Logo JQuery"/>
             <h3 className="mb-3">JQuery</h3>
           </div>
         </div>
         <div className="col-lg-3 col-md-6 text-center">
-          <div className="service-box mt-5 mx-auto">
-            <img className="sr-button" src="images/icons/react.png" height="100" alt="Logo REACT.js"/>
+          <div className="sr-button service-box mt-5 mx-auto">
+            <img src="images/icons/react.png" height="100" alt="Logo REACT.js"/>
             <h3 className="mb-3">React.js</h3>
           </div>
         </div>
         <div className="col-lg-3 col-md-6 text-center">
-          <div className="service-box mt-5 mx-auto">
-            <img className="sr-button" src="images/icons/node.png" height="100" alt="Logo node.js"/>
-            <h3 className="mb-3">React.js</h3>
+          <div className="sr-button service-box mt-5 mx-auto">
+            <img src="images/icons/node.png" height="100" alt="Logo node.js"/>
+            <h3 className="mb-3">Node.js</h3>
           </div>
         </div>
       </div>
