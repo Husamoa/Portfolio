c010aebffb6d4c4d031c26685ec137d896f6c4b1
Husamoa <pawebial@gmail.com>
2019-02-10 20:48:13 +0100

    another tooltip on mobile

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -18,104 +18,160 @@ const Technologies = () => {
         <div className="row">
           <div className="col-lg-3 col-md-6 text-center">
             <div className="sr-button mt-5 mx-auto" data-tip data-for="html">
-              <ReactTooltip id="html" place="top" type="light" effect="solid">
+              <ReactTooltip className="tooltip" id="html" place="top" type="light" effect="solid">
                 <FontAwesomeIcon icon={faStar} className="checked"/>
                 <FontAwesomeIcon icon={faStar} className="checked"/>
                 <FontAwesomeIcon icon={faStar} className="checked"/>
                 <FontAwesomeIcon icon={faStar}/>
                 <FontAwesomeIcon icon={faStar}/>
               </ReactTooltip>
+              <div className="tooltip-mobile">
+                <FontAwesomeIcon icon={faStar} className="checked"/>
+                <FontAwesomeIcon icon={faStar} className="checked"/>
+                <FontAwesomeIcon icon={faStar} className="checked"/>
+                <FontAwesomeIcon icon={faStar}/>
+                <FontAwesomeIcon icon={faStar}/>
+              </div>
               <img src="images/icons/html.png" height="100"/>
               <h3 className="mb-3">HTML5</h3>
             </div>
           </div>
           <div className="col-lg-3 col-md-6 text-center">
             <div className="sr-button mt-5 mx-auto" data-tip data-for="css">
-              <ReactTooltip id="css" place="top" type="light" effect="solid">
+              <ReactTooltip className="tooltip" id="css" place="top" type="light" effect="solid">
                 <FontAwesomeIcon icon={faStar} className="checked"/>
                 <FontAwesomeIcon icon={faStar} className="checked"/>
                 <FontAwesomeIcon icon={faStar} className="checked"/>
                 <FontAwesomeIcon icon={faStar}/>
                 <FontAwesomeIcon icon={faStar}/>
               </ReactTooltip>
+              <div className="tooltip-mobile">
+                <FontAwesomeIcon icon={faStar} className="checked"/>
+                <FontAwesomeIcon icon={faStar} className="checked"/>
+                <FontAwesomeIcon icon={faStar} className="checked"/>
+                <FontAwesomeIcon icon={faStar}/>
+                <FontAwesomeIcon icon={faStar}/>
+              </div>
               <img src="images/icons/css.png" height="100" alt="Logo CSS"/>
               <h3 className="mb-3">CSS 3</h3>
             </div>
           </div>
           <div className="col-lg-3 col-md-6 text-center">
             <div className="sr-button mt-5 mx-auto" data-tip data-for="js">
-              <ReactTooltip id="js" place="top" type="light" effect="solid">
+              <ReactTooltip className="tooltip" id="js" place="top" type="light" effect="solid">
                 <FontAwesomeIcon icon={faStar} className="checked"/>
                 <FontAwesomeIcon icon={faStar} className="checked"/>
                 <FontAwesomeIcon icon={faStar} className=""/>
                 <FontAwesomeIcon icon={faStar}/>
                 <FontAwesomeIcon icon={faStar}/>
               </ReactTooltip>
+              <div className="tooltip-mobile">
+                <FontAwesomeIcon icon={faStar} className="checked"/>
+                <FontAwesomeIcon icon={faStar} className="checked"/>
+                <FontAwesomeIcon icon={faStar} className=""/>
+                <FontAwesomeIcon icon={faStar}/>
+                <FontAwesomeIcon icon={faStar}/>
+              </div>
               <img src="images/icons/js.png" height="100" alt="Logo JavaScript"/>
               <h3 className="mb-3">JavaScript</h3>
             </div>
           </div>
           <div className="col-lg-3 col-md-6 text-center">
             <div className="sr-button mt-5 mx-auto" data-tip data-for="react">
-              <ReactTooltip id="react" place="top" type="light" effect="solid">
+              <ReactTooltip className="tooltip" id="react" place="top" type="light" effect="solid">
                 <FontAwesomeIcon icon={faStar} className="checked"/>
                 <FontAwesomeIcon icon={faStar} className="checked"/>
                 <FontAwesomeIcon icon={faStar} className=""/>
                 <FontAwesomeIcon icon={faStar}/>
                 <FontAwesomeIcon icon={faStar}/>
               </ReactTooltip>
+              <div className="tooltip-mobile">
+                <FontAwesomeIcon icon={faStar} className="checked"/>
+                <FontAwesomeIcon icon={faStar} className="checked"/>
+                <FontAwesomeIcon icon={faStar} className=""/>
+                <FontAwesomeIcon icon={faStar}/>
+                <FontAwesomeIcon icon={faStar}/>
+              </div>
               <img src="images/icons/react.png" height="100" alt="Logo REACT.js"/>
               <h3 className="mb-3">React.js</h3>
             </div>
           </div>
           <div className="col-lg-3 col-md-6 text-center">
             <div className="sr-button mt-5 mx-auto" data-tip data-for="jquery">
-              <ReactTooltip id="jquery" place="top" type="light" effect="solid">
+              <ReactTooltip className="tooltip" id="jquery" place="top" type="light" effect="solid">
                 <FontAwesomeIcon icon={faStar} className="checked"/>
                 <FontAwesomeIcon icon={faStar} className="checked"/>
                 <FontAwesomeIcon icon={faStar} className=""/>
                 <FontAwesomeIcon icon={faStar}/>
                 <FontAwesomeIcon icon={faStar}/>
               </ReactTooltip>
+              <div className="tooltip-mobile">
+                <FontAwesomeIcon icon={faStar} className="checked"/>
+                <FontAwesomeIcon icon={faStar} className="checked"/>
+                <FontAwesomeIcon icon={faStar} className=""/>
+                <FontAwesomeIcon icon={faStar}/>
+                <FontAwesomeIcon icon={faStar}/>
+              </div>
               <img src="images/icons/jquery.png" height="100" alt="Logo JQuery"/>
               <h3 className="mb-3">JQuery</h3>
             </div>
           </div>
           <div className="col-lg-3 col-md-6 text-center">
             <div className="sr-button mt-5 mx-auto" data-tip data-for="sass">
-              <ReactTooltip id="sass" place="top" type="light" effect="solid">
+              <ReactTooltip className="tooltip" id="sass" place="top" type="light" effect="solid">
                 <FontAwesomeIcon icon={faStar} className="checked"/>
                 <FontAwesomeIcon icon={faStar} className="checked"/>
                 <FontAwesomeIcon icon={faStar} className="checked"/>
                 <FontAwesomeIcon icon={faStar}/>
                 <FontAwesomeIcon icon={faStar}/>
               </ReactTooltip>
+              <div className="tooltip-mobile">
+                <FontAwesomeIcon icon={faStar} className="checked"/>
+                <FontAwesomeIcon icon={faStar} className="checked"/>
+                <FontAwesomeIcon icon={faStar} className="checked"/>
+                <FontAwesomeIcon icon={faStar}/>
+                <FontAwesomeIcon icon={faStar}/>
+              </div>
               <img src="images/icons/sass.png" height="100" alt="Logo SASS"/>
               <h3 className="mb-3">SASS</h3>
             </div>
           </div>
           <div className="col-lg-3 col-md-6 text-center">
             <div className="sr-button mt-5 mx-auto" data-tip data-for="bootstrap">
-              <ReactTooltip id="bootstrap" place="top" type="light" effect="solid">
+              <ReactTooltip className="tooltip" id="bootstrap" place="top" type="light" effect="solid">
                 <FontAwesomeIcon icon={faStar} className="checked"/>
                 <FontAwesomeIcon icon={faStar} className="checked"/>
                 <FontAwesomeIcon icon={faStar} className="checked"/>
                 <FontAwesomeIcon icon={faStar}/>
                 <FontAwesomeIcon icon={faStar}/>
               </ReactTooltip>
+              <div className="tooltip-mobile">
+                <FontAwesomeIcon icon={faStar} className="checked"/>
+                <FontAwesomeIcon icon={faStar} className="checked"/>
+                <FontAwesomeIcon icon={faStar} className="checked"/>
+                <FontAwesomeIcon icon={faStar}/>
+                <FontAwesomeIcon icon={faStar}/>
+              </div>
               <img src="images/icons/bootstrap.png" height="100" alt="Logo Bootstrap"/>
               <h3 className="mb-3">Bootstrap</h3>
             </div>
           </div>
           <div className="col-lg-3 col-md-6 text-center">
             <div className="sr-button mt-5 mx-auto" data-tip data-for="node">
-              <ReactTooltip id="node" place="top" type="light" effect="solid">
+              <ReactTooltip className="tooltip" id="node" place="top" type="light" effect="solid">
                 <FontAwesomeIcon icon={faStar} className="checked"/>
                 <FontAwesomeIcon icon={faStar} className=""/>
                 <FontAwesomeIcon icon={faStar} className=""/>
                 <FontAwesomeIcon icon={faStar}/>
                 <FontAwesomeIcon icon={faStar}/>
               </ReactTooltip>
+              <div className="tooltip-mobile">
+                <FontAwesomeIcon icon={faStar} className="checked"/>
+                <FontAwesomeIcon icon={faStar} className=""/>
+                <FontAwesomeIcon icon={faStar} className=""/>
+                <FontAwesomeIcon icon={faStar}/>
+                <FontAwesomeIcon icon={faStar}/>
+              </div>
               <img src="images/icons/node.png" height="100" alt="Logo node.js"/>
               <h3 className="mb-3">Node.js</h3>
             </div>
