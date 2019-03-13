98f05ba8d9d44e29d6fc4fe8383b52892be73dc8
Husamoa <pawebial@gmail.com>
2019-03-13 16:47:06 +0100

    changing photo and text about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,9 +14,9 @@ const About = () => {
               <hr/>
               </div>
               <div className="col-xl-8 mx-auto text-center">
-                <p className="text-faded mb-5">Hi! I am a 31-year-old front-end developer living in Poznań, Poland. I started my adventure with web development in the November of 2017. In my projects I mainly use technologies like: HTML5, CSS3 (SASS), JavaScript, ReactJS. I am currently expanding my knowledge about JavaScript and ReactJS. I also create responsive websites, so they display correctly from the smallest to the largest screen. I think Bootstrap and ReactJS are awesome :)</p>
+                <p className="text-faded mb-5">Hi! My name is Paweł. I am a 31-year-old front-end developer living in Poznań, Poland. I started my adventure with web development in the November of 2017. In my projects I mainly use technologies like: HTML5, CSS3 (SASS), JavaScript, ReactJS. I am currently expanding my knowledge about JavaScript and ReactJS. I also create responsive websites, so they display correctly from the smallest to the largest screen. I think Bootstrap and ReactJS are awesome :)</p>
 
-                <p>I am looking for a job in Poznań or remote as a junior front-end developer. If you are interested in hiring me or cooperating with me, please go to the contact me section below.</p>
+                <p>I am looking for a job in Poznań or remote as a junior front-end developer. If you are interested in hiring me or cooperating with me, please contact me.</p>
                 <NavLink className="btn btn-warning text-white" to="/contact"> Contact </NavLink>
               </div>
             </div>
